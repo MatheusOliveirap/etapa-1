e8027f62b7fbb052b7b7e8d01e1fdd36579e70b9
MatheusOliveiraP <oliveiramatheus21322@gmail.com>
2026-04-09 22:08:42 -0300

    etapa 1

---
+++ src/br/ulbra/dao/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.ulbra.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/ti_escola",
+                "root",
+                ""
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na conexão: " + e.getMessage());
+        }
+    }
+}
---
+++ src/br/ulbra/model/Chamado.java
@@ -0,0 +1,45 @@
+package br.ulbra.model;
+
+import java.sql.Timestamp;
+
+public class Chamado {
+
+    private long id;
+    private String solicitante;
+    private String sala;
+    private String equipamentoTag;
+    private String problemaRelatado;
+    private String diagnosticoTecnico;
+    private String prioridade;
+    private String status;
+    private Timestamp dataAbertura;
+
+    // GETTERS E SETTERS
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getSolicitante() { return solicitante; }
+    public void setSolicitante(String solicitante) { this.solicitante = solicitante; }
+
+    public String getSala() { return sala; }
+    public void setSala(String sala) { this.sala = sala; }
+
+    public String getEquipamentoTag() { return equipamentoTag; }
+    public void setEquipamentoTag(String equipamentoTag) { this.equipamentoTag = equipamentoTag; }
+
+    public String getProblemaRelatado() { return problemaRelatado; }
+    public void setProblemaRelatado(String problemaRelatado) { this.problemaRelatado = problemaRelatado; }
+
+    public String getDiagnosticoTecnico() { return diagnosticoTecnico; }
+    public void setDiagnosticoTecnico(String diagnosticoTecnico) { this.diagnosticoTecnico = diagnosticoTecnico; }
+
+    public String getPrioridade() { return prioridade; }
+    public void setPrioridade(String prioridade) { this.prioridade = prioridade; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public Timestamp getDataAbertura() { return dataAbertura; }
+    public void setDataAbertura(Timestamp dataAbertura) { this.dataAbertura = dataAbertura; }
+}
